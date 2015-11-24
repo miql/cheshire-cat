447c66bc7ee289df1762d1a8da210b7dfac504f5
miql <mxander@gmail.com>
2015-11-23 23:57:43 -0700

    Adding clojurescript

--- project.clj
+++ project.clj
@@ -3,11 +3,23 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [ring/ring-json "0.3.1"]
+                 [cljs-http "0.1.18"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [enfocus "2.1.0"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.3"]]
   :ring {:handler cheshire-cat.handler/app}
+  :cljsbuild {
+      :builds [{
+        :source-paths ["src-cljs"]
+        :compiler {
+          :output-to "resources/public/main.js"
+          :optimizations :whitespace
+          :pretty-print true }}]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
---
+++ src-cljs/core.cljs
@@ -0,0 +1,11 @@
+(ns cheshire-cat.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [clojure.browser.repl :as repl]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(defn ^:export init []
+  (repl/connect "http://localhost:9000/repl")
+  (go
+    (let [response (<! (http/get "/cheshire-cat"))]
+         (js/alert (:body response)))))
